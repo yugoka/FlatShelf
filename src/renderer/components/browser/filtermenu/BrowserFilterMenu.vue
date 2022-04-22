e3f361f7a75856d5a815b5ee30e9995fd9c34106
Asachuna <84565188+Asachuna@users.noreply.github.com>
2022-04-23 02:03:27 +0900

    [add]フィルターメニューを形だけ追加

---
+++ src/renderer/components/browser/filtermenu/BrowserFilterMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-system-bar
+    class="filtermenu elevation-3"
+    color="systemBar"
+    height="40"
+  >
+    aaa
+  </v-system-bar>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.filtermenu {
+  position: relative;
+}
+</style>
--- src/renderer/views/Browser.vue
+++ src/renderer/views/Browser.vue
@@ -1,92 +1,113 @@
 <template>
   <div
     class="main-browser-wrapper"
-    @dragenter.prevent
-    @dragover.prevent="onDragOver"
-    @dragleave="fileDropSnackBar = false"
-    @drop="onDrop"
   >
-    <!-- 上部のツールバー -->
-    <BrowserToolbar/>
-
-    <!-- 検索画面本体 -->
-    <SearchContents
-      ref="contents"
+    <BrowserFilterMenu
+      class="filtermenu"
     />
 
-    <!-- ファイルドロップ用スナックバー -->
-    <v-snackbar
-      v-model="fileDropSnackBar"
-      color="primary"
-      content-class="text-center"
-      timeout="-1"
-      transition="slide-y-reverse-transition"
+    <div
+      class="search-wrapper"
+      @dragenter.prevent
+      @dragover.prevent="onDragOver"
+      @dragleave="fileDropSnackBar = false"
+      @drop="onDrop"
     >
-      <div>
-        <v-icon large>mdi-import</v-icon>
-      </div>
-      ファイルをドロップしてインポート
-    </v-snackbar>
+      <!-- 上部のツールバー -->
+      <BrowserToolbar/>
 
+      <!-- 検索画面本体 -->
+      <SearchContents
+        ref="contents"
+      />
+
+      <!-- ファイルドロップ用スナックバー -->
+      <v-snackbar
+        v-model="fileDropSnackBar"
+        color="primary"
+        content-class="text-center"
+        timeout="-1"
+        transition="slide-y-reverse-transition"
+      >
+        <div>
+          <v-icon large>mdi-import</v-icon>
+        </div>
+        ファイルをドロップしてインポート
+      </v-snackbar>
+
+    </div>
   </div>
 </template>
 
 <script>
-  import SearchContents from "../components/browser/Search"
-  import BrowserToolbar from "../components/browser/BrowserToolbar.vue"
+import SearchContents from "../components/browser/Search"
+import BrowserToolbar from "../components/browser/BrowserToolbar.vue"
+import BrowserFilterMenu from "../components/browser/filtermenu/BrowserFilterMenu.vue"
 
-  export default {
+export default {
 
-    name:"Browser",
+  name:"Browser",
 
-    components: {
+  components: {
     SearchContents,
     BrowserToolbar,
-    },
+    BrowserFilterMenu
+  },
 
-    data() {
-      return {
-        fileDropSnackBar: false,
-        isDraggingFile: false
-      }
-    },
-
-    methods: {
-      //ファイルをドラッグしてマウスオーバーした時の処理
-      onDragOver(event) {
-        if (!event.dataTransfer.types.includes("Files")) return
-        this.fileDropSnackBar = true
-      },
-
-      //ファイルドロップ時の処理
-      async onDrop(event) {
-        this.fileDropSnackBar = false
-        if (!event.dataTransfer.types.includes("Files")) return
-        
-        const files = event.dataTransfer.files
-        const context = this.$store.state.viewContext
-        //コンテキストにフォルダが設定されていない場合未分類にインポートする
-        const folderID = context.folder
-          ? context.folder
-          : 1
-
-        await this.$contents.createMany({
-          files,
-          folderID
-        })
-      }
+  data() {
+    return {
+      fileDropSnackBar: false,
+      isDraggingFile: false
     }
+  },
 
+  methods: {
+    //ファイルをドラッグしてマウスオーバーした時の処理
+    onDragOver(event) {
+      if (!event.dataTransfer.types.includes("Files")) return
+      this.fileDropSnackBar = true
+    },
 
+    //ファイルドロップ時の処理
+    async onDrop(event) {
+      this.fileDropSnackBar = false
+      if (!event.dataTransfer.types.includes("Files")) return
+      
+      const files = event.dataTransfer.files
+      const context = this.$store.state.viewContext
+      //コンテキストにフォルダが設定されていない場合未分類にインポートする
+      const folderID = context.folder
+        ? context.folder
+        : 1
+
+      await this.$contents.createMany({
+        files,
+        folderID
+      })
+    }
   }
+
+
+}
 
 </script>
 
 <style scoped>
+/* システムメニューとフィルターメニューに対しハードコーディング */
 .main-browser-wrapper {
-  height: calc(100vh - 30px);
   position: relative;
+  height: calc(100vh - 30px);
 }
 
+.filtermenu {
+  position: relative;
+  z-index: 2;
+}
+
+.search-wrapper {
+  position: relative;
+  z-index: 1;
+  height: calc(100vh - 70px);
+}
 
 </style>
